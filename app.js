b0c9911661f697c6ddc9821d29693422d340a05a
MBlancher <redcomet120@gmail.com>
2015-04-26 16:57:34 -0700

    started adding code to use passport for user auth

--- app.js
+++ app.js
@@ -1,34 +1,57 @@
 'use strict';
 
 global.root_require = function(name) {
-	return require(__dirname + '/' + name);
+    return require(__dirname + '/' + name);
 };
 //required modules
 var express = require('express');
+
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
 var app = express();
+
+//Configure
+passport.use(new LocalStrategy(
+        function(usr, pword, done){
+            User.findOne({ username: usr }, function (err, user) {
+                if(err){ return done(err);}
+                if(!user){
+                    return done(null, false, { message: 'Incorrect username. '});
+                }
+                if(!user.validPassword(pword)){
+                    return done(null, false, { message: 'Wrong password. '});
+                }
+                return done(null, user);
+            });
+        }));
 
 //redirect ??
 app.get('/', function(req, res) {
-    res.redirect('dongs');
+    res.redirect('login');
 });
 
-// rout to index ? 
+// login local auth redirect
+app.post('/login',
+    passport.authenticate('local'),
+        {sucessRedirect: '/',
+        failureRedirect: '/login',
+    failureFlash: true})
+);
+
+// route to index ?
 app.get('/dongs', function(req, res) {
-	var html = __dirname + '/static/index.html';
-	res.status(200)
-		.set('Content-Type', 'text/html')
-		.sendFile(html);
+    var html = __dirname + '/static/index.html';
+    res.status(200)
+    .set('Content-Type', 'text/html')
+    .sendFile(html);
 });
 
 //starts the server listening
-var server = app.listen(8083, '192.168.1.12');
-console.log("its's running on 8080");
-/*
-var server = app.listen(8080, function() {
+var server = app.listen(8083, function() {
     var host = server.address().address;
     var port = server.address().port;
 
     console.log('Example app listening at http://%s:%s', host, port);
 });
-*/
+
 module.exports = app;
---
+++ core/auth.js
@@ -0,0 +1,2 @@
+//==================== Auth module=========
+var auth = require("passport")
