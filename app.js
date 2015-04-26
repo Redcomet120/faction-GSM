45163f61a9faa2868d452d2c785df000bd0ac7ec
MBlancher <redcomet120@gmail.com>
2015-04-26 13:18:32 -0700

    hard coded the address of the server and changed the port to try to resolve launching issue

--- app.js
+++ app.js
@@ -3,26 +3,32 @@
 global.root_require = function(name) {
 	return require(__dirname + '/' + name);
 };
-
+//required modules
 var express = require('express');
 var app = express();
 
+//redirect ??
 app.get('/', function(req, res) {
-    res.redirect('bacon');
+    res.redirect('dongs');
 });
 
-app.get('/bacon', function(req, res) {
+// rout to index ? 
+app.get('/dongs', function(req, res) {
 	var html = __dirname + '/static/index.html';
 	res.status(200)
 		.set('Content-Type', 'text/html')
 		.sendFile(html);
 });
 
-var server = app.listen(25565, function() {
+//starts the server listening
+var server = app.listen(8083, '192.168.1.12');
+console.log("its's running on 8080");
+/*
+var server = app.listen(8080, function() {
     var host = server.address().address;
     var port = server.address().port;
 
     console.log('Example app listening at http://%s:%s', host, port);
 });
-
+*/
 module.exports = app;
