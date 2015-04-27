b571ca8670e51383f022c0dd64da4127a1623c77
MBlancher <redcomet120@gmail.com>
2015-04-26 19:21:54 -0700

    More working functions for login need to figure out what is going on with Get request

--- app.js
+++ app.js
@@ -5,12 +5,39 @@ global.root_require = function(name) {
 };
 //required modules
 var express = require('express');
-
 var passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy;
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var session = require('express-session');
+
 var app = express();
 
-//Configure
+// Configure express
+    app.use(express.static('static'));
+    app.use(logger('combined'));
+    app.use(cookieParser());
+    app.use(bodyParser.urlencoded({ extended: false}));
+    app.use(bodyParser.json());
+    app.use(methodOverride('X-HTTP-Method-Override'));
+    app.use(session({ secret: 'baconTech', saveUninitialized: true, resave: true}));
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+//create a toekn cookie for user
+passport.serializeUser(function(user, done) {
+      done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+      User.findById(id, function(err, user) {
+              done(err, user);
+                });
+});
+
+//Configure passport strategy
 passport.use(new LocalStrategy(
         function(usr, pword, done){
             User.findOne({ username: usr }, function (err, user) {
@@ -24,7 +51,6 @@ passport.use(new LocalStrategy(
                 return done(null, user);
             });
         }));
-
 //redirect ??
 app.get('/', function(req, res) {
     res.redirect('login');
@@ -32,11 +58,11 @@ app.get('/', function(req, res) {
 
 // login local auth redirect
 app.post('/login',
-    passport.authenticate('local'),
-        {sucessRedirect: '/',
+    passport.authenticate('local',
+        {successRedirect: '/',
         failureRedirect: '/login',
-    failureFlash: true})
-);
+    failureFlash: true}
+));
 
 // route to index ?
 app.get('/dongs', function(req, res) {
