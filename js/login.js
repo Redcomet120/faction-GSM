bd940ec1020d88b25d65fdd872500397e9741ed1
MBlancher <redcomet120@gmail.com>
2015-05-03 00:46:51 -0700

    added the local strategy to auth and modified login still need to setup hashing.

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 var Backbone = require('backbone');
 var $ = require('jquery');
+var _ = require('lodash');
 var React = require('react');
 
 window.Backbone = Backbone;
@@ -8,15 +9,39 @@ window.$ = $;
 var Login = React.createClass({
     displayName: 'Login',
 
+    login: function(e) {
+        e.preventDefault();
+        var target = $(e.target);
+        var labels = target.siblings('label input');
+        _.each(labels, function(label) {
+            console.log($(label).val());
+        });
+    },
     render: function() {
-        /* jshint ignore:start */
         return (
-            <div>This will be the login page</div>
+            <div>This will be the login page
+                <div className="row">
+                    <div className="small-12 columns">
+                        <form>
+                            <label>
+                              <span>Username:</span>
+                                <input type="text" />
+                            </label>
+                            <label>
+                                <span>Password:</span>
+                                <input type="password" />
+                            </label>
+                            <button onClick={this.login}>Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         );
-        /* jshint ignore:end */
     }
 });
 
-/* jshint ignore:start */
+//styling login box
+var divStyle = {
+    color:'red'
+};
 React.render(<Login />, document.getElementById('content'));
-/* jshint ignore:end */
