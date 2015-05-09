85691772013f0dec4dc9aab7d8216ca4b3363236
MBlancher <redcomet120@gmail.com>
2015-05-08 22:30:25 -0700

    working on logins

--- js/login/index.jsx
+++ js/login/index.jsx
@@ -10,6 +10,12 @@ window.Backbone.$ = $;
 var Login = React.createClass({
     displayName: 'Login',
 
+    getInitialState: function() {
+        return {
+            signup: false
+        };
+    },
+
     login: function(e) {
         e.preventDefault();
         var target = $(e.target);
@@ -20,21 +26,54 @@ var Login = React.createClass({
 
         Backbone.trigger('login', credentials);
     },
+
+    signup: function(e) {
+        e.preventDefault();
+        var target = $(e.target);
+        var credentials = {
+            username: target.siblings('#username').children('input').val(),
+            password: target.siblings('#password').children('input').val()
+        };
+
+        Backbone.trigger('signup', credentials);
+    },
+
+    getSignup: function() {
+        return (
+            <form style={{display: this.state.signup ? 'block' : 'none'}}>
+                <label className="username">
+                    <span>Username:</span>
+                    <input type="text" />
+                </label>
+                <label className="password">
+                    <span>Password:</span>
+                    <input type="password" />
+                </label>
+                <button onClick={this.signup}>Signup</button>
+            </form>
+        );
+    },
+    getLogin: function() {
+        return (
+            <form style={{display: this.state.signup ? 'none' : 'block'}}>
+                <label className="username">
+                    <span>Username:</span>
+                    <input type="text" />
+                </label>
+                <label className="password">
+                    <span>Password:</span>
+                    <input type="password" />
+                </label>
+                <button onClick={this.login}>Login</button>
+            </form>
+        );
+    },
     render: function() {
         return (
             <div className="row">
                 <div className="small-12 columns">
-                    <form>
-                        <label id="username">
-                            <span>Username:</span>
-                            <input type="text" />
-                        </label>
-                        <label id="password">
-                            <span>Password:</span>
-                            <input type="password" />
-                        </label>
-                        <button onClick={this.login}>Login</button>
-                    </form>
+                    {this.getLogin()}
+                    {this.getSignup()}
                 </div>
             </div>
         );
